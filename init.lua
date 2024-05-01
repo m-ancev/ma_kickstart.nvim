0ea4be9e6e507bfdbfd21d3ac72a045d36555a85
m-ancev <m.ancev04@gmail.com>
2024-05-01 14:23:19 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -120,7 +120,7 @@ vim.opt.breakindent = true
 
 -- Save undo history
 vim.opt.undofile = true
-vim.o.undodir = "~/.local/state/nvim/undo/"
+vim.o.undodir = '~/.local/state/nvim/undo/'
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
