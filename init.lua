ff3cb2a674beaed0b66061da870e26bf1b20ef86
m-ancev <m.ancev04@gmail.com>
2024-06-28 14:46:14 +0200

    Add ~/Desktop as default path when launching neovim
    Should be ~ on Linux

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    vim.cmd 'lcd ~/Desktop'
+  end,
+})
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
