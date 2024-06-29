c784c0981c86a08b609f7c2a24b640f2f85f7252
m-ancev <m.ancev04@gmail.com>
2024-06-30 01:13:47 +0200

    Move plugins from kickstart to plugin dir

--- init.lua
+++ init.lua
@@ -655,9 +655,9 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'plugins.debug',
+  require 'plugins.autopairs',
+  require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
